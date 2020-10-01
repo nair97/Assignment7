6061208caeb148933e7067ebbc0f77f8bc883f08
nair97 <70231014+nair97@users.noreply.github.com>
2020-10-01 16:07:10 -0400

    spatialdatadone
    spatialdatadone

--- Spatial_Data_Analysis.py
+++ Spatial_Data_Analysis.py
@@ -87,66 +87,60 @@ def compute_average_moisture_by_station( DataDF, MetaDF ):
 # the following condition checks whether we are running as a script, in which 
 # case run the test code, otherwise functions are being imported so do not.
 # put the main routines from your code after this conditional check.
-import pandas as pd
-if __name__ == '__main__': 
- import matplotlib.pyplot as plt
- import cartopy.crs as ccrs
- import cartopy.feature as cfeature
- import matplotlib.cm as cm
- from mpl_toolkits.axes_grid1 import ImageGrid
- from cartopy.mpl.geoaxes import GeoAxes
 
+if __name__ == '__main__': 
+ 
     # Open and read the contents of the metadata file
-stnInfoFileName = 'datasets/ill-station-info.csv'
-stnInfo = read_station_info( stnInfoFileName )
+    stnInfoFileName = 'datasets/ill-station-info.csv'
+    stnInfo = read_station_info( stnInfoFileName )
     
     # Open and read the contents of the data file
-stationDataFileName = 'datasets/ill-soilmoist-data.txt'
-stationData = read_station_data( stationDataFileName )
+    stationDataFileName = 'datasets/ill-soilmoist-data.txt'
+    stationData = read_station_data( stationDataFileName )
     
     # Compute total soil column soil moisture
-stationData = compute_total_moisture( stationData )
+    stationData = compute_total_moisture( stationData )
         
     # Compute annual averages
-annualAvg = compute_average_moisture_by_station( stationData, stnInfo )
+    annualAvg = compute_average_moisture_by_station( stationData, stnInfo )
 
     # set bounds for Illinois
-lon1, lon2, lat1, lat2 = -93, -86, 36, 44 # illinois (left, right, bottom, top)
+    lon1, lon2, lat1, lat2 = -93, -86, 36, 44 # illinois (left, right, bottom, top)
     
     #
     # make map of annual average total soil moisture values
     #
-fig = plt.figure(figsize=(10,10))
-ax = fig.add_subplot(1,1,1, projection = ccrs.UTM('16N')) #setting projection to UTM 16N
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(1,1,1, projection = ccrs.UTM('16N')) #setting projection to UTM 16N
     
-ax.add_feature(cfeature.COASTLINE, edgecolor="black")#set coastline 
-ax.add_feature(cfeature.BORDERS, edgecolor="black")#set borders
-ax.add_feature(cfeature.STATES, edgecolor="black")#set US states
-ax.set_extent([lon1, lon2, lat1, lat2], crs=ccrs.PlateCarree())
-col = annualAvg["Annual Total Water Depth (mm)"].values #set to column values
+    ax.add_feature(cfeature.COASTLINE, edgecolor="black")#set coastline 
+    ax.add_feature(cfeature.BORDERS, edgecolor="black")#set borders
+    ax.add_feature(cfeature.STATES, edgecolor="black")#set US states
+    ax.set_extent([lon1, lon2, lat1, lat2], crs=ccrs.PlateCarree())
+    col = annualAvg["Annual Total Water Depth (mm)"].values #set to column values
    
     #plot Annual Total Water depth data in scatter plot
-plt.scatter(x=annualAvg.Lon, y=annualAvg.Lat,
+    plt.scatter(x=annualAvg.Lon, y=annualAvg.Lat,
                 c=(col),cmap='jet',  
                 s=500, alpha=1, edgecolors = "black", 
                 transform=ccrs.PlateCarree()
                )
-plt.colorbar(label=" in mm ") # set color bar 
-fig.suptitle('Annual Total Water Depth (mm) in Illinois', fontsize = 16)#map title
-plt.savefig('Annual Total Water Depth (mm).PNG') #save plot
-plt.show() #show plot
-plt.close() #close plot
+    plt.colorbar(label=" in mm ") # set color bar 
+    fig.suptitle('Annual Total Water Depth (mm) in Illinois', fontsize = 16)#map title
+    plt.savefig('Annual Total Water Depth (mm).PNG') #save plot
+    plt.show() #show plot
+    plt.close() #close plot
     
     # Make 4 plot panel with seasonal average soil moisture as VWC
 
 
-projection = ccrs.UTM('16N')
-axes_class = (GeoAxes,
+    projection = ccrs.UTM('16N')
+    axes_class = (GeoAxes,
                   dict(map_projection=projection))
     
-fig = plt.figure(figsize=(6,6))
+    fig = plt.figure(figsize=(6,6))
     # establish 2x2 grid for plots    
-grid = ImageGrid(fig, 111, axes_class=axes_class,
+    grid = ImageGrid(fig, 111, axes_class=axes_class,
                  nrows_ncols=(2, 2),  # creates 2x2 grid of axes
                  axes_pad=0.2,  # pad between axes in inches
                  cbar_location='right',
@@ -156,11 +150,11 @@ grid = ImageGrid(fig, 111, axes_class=axes_class,
                  label_mode='')
     
         # make your plot here for the current season        
-season = (annualAvg['Winter Total VWC (%)'], annualAvg['Spring Total VWC (%)'],
+    season = (annualAvg['Winter Total VWC (%)'], annualAvg['Spring Total VWC (%)'],
                 annualAvg['Summer Total VWC (%)'], annualAvg['Fall Total VWC (%)'])
        
           # loop through the four panels, and the four seasons
-for sidx, ax in enumerate(grid):
+    for sidx, ax in enumerate(grid):
         ax.coastlines()
         ax.add_feature(cfeature.BORDERS)
         ax.add_feature(cfeature.STATES)
@@ -173,11 +167,11 @@ for sidx, ax in enumerate(grid):
                        transform=ccrs.PlateCarree())
         
         
-cbar = grid.cbar_axes[0].colorbar(p)
-cbar.set_label_text("Total VWC (%)")
+        cbar = grid.cbar_axes[0].colorbar(p)
+        cbar.set_label_text("Total VWC (%)")
 
        #save plot and close figure
-plt.savefig('TotalVWC.PNG') #save plot
-plt.show() #show plot
-plt.close() #close plot 
+    plt.savefig('TotalVWC.PNG') #save plot
+    plt.show() #show plot
+    plt.close() #close plot 
        
